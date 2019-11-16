22c8936512e6828ecda9269a50f1c0f74b060b55
Adroaldo de Andrade <adroaldof@gmail.com>
2019-11-16 12:32:56 -0300

    Add docker compose and some other configs

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -1,9 +1,13 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
 export const typeOrmConfig: TypeOrmModuleOptions = {
-  type: 'sqlite',
-  database: 'database.sqlite',
+  type: process.env.DB_DRIVER || 'postgres',
+  host: process.env.DB_HOST || 'localhost',
+  port: process.env.DB_PORT || 5432,
+  username: process.env.DB_USER || 'postgres',
+  password: process.env.DB_PASSWORD || 'postgres',
+  database: process.env.DB_NAME || 'postgres',
   synchronize: true,
   logging: true,
   entities: [__dirname + '/../**/*.entity.{js,ts}'],
-};
+} as TypeOrmModuleOptions;

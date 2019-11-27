3c32620323f385f4a3beb3a7a3da1fa9347655b0
Adroaldo de Andrade <adroaldof@gmail.com>
2019-11-26 21:06:57 -0300

    Add upload single and multiple files

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,7 +1,12 @@
+import { diskStorage } from 'multer';
+
 import {
-    Controller, Get, Logger, Param, Post, Res, UploadedFile, UseInterceptors
+    Controller, Get, Logger, Param, Post, Res, UploadedFile, UploadedFiles, UseInterceptors
 } from '@nestjs/common';
-import { FileInterceptor } from '@nestjs/platform-express';
+import { FileInterceptor, FilesInterceptor } from '@nestjs/platform-express';
+
+import { appHost, appPort } from './config/constants';
+import { fileUploadFilter, fileUploadRename } from './utils/file-upload';
 
 @Controller()
 export class AppController {
@@ -10,9 +15,51 @@ export class AppController {
   constructor() {}
 
   @Post('upload')
-  @UseInterceptors(FileInterceptor('file'))
+  @UseInterceptors(
+    FileInterceptor('file', {
+      fileFilter: fileUploadFilter,
+      storage: diskStorage({
+        destination: 'uploads',
+        filename: fileUploadRename,
+      }),
+    }),
+  )
   uploadFile(@UploadedFile() file) {
     this.logger.debug(file);
+    const { originalname, filename, size } = file;
+
+    return {
+      originalname,
+      filename,
+      size,
+      path: `${appHost}:${appPort}/image/${filename}`,
+    };
+  }
+
+  @Post('upload-multiple')
+  @UseInterceptors(
+    FilesInterceptor('files', 5, {
+      fileFilter: fileUploadFilter,
+      storage: diskStorage({
+        destination: 'uploads',
+        filename: fileUploadRename,
+      }),
+    }),
+  )
+  uploadFiles(@UploadedFiles() files) {
+    this.logger.debug(files);
+    const uploaded = [];
+
+    files.forEach(({ originalname, filename, size }) =>
+      uploaded.push({
+        originalname,
+        filename,
+        size,
+        path: `${appHost}:${appPort}/image/${filename}`,
+      }),
+    );
+
+    return uploaded;
   }
 
   @Get('image/:path')
